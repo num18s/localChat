7989bc649c797d145c0232b50fc9af0cf72578ad
num18s <bohaos@gmail.com>
2013-07-15 18:47:48 -0700

    initial post

---
+++ localChat/ReadPanorama.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace localChat
+{
+    public partial class ReadPanorama : PhoneApplicationPage
+    {
+        public ReadPanorama()
+        {
+            InitializeComponent();
+        }
+
+        private void CurrentLB_SelectionChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("CurrentLB Selected");
+            //Do work for your application here.
+        }
+
+        private void ListBox_SelectionChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("CurrentLB Selected");
+            //Do work for your application here.
+        }       
+        private void Write_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Write button works!");
+            //Do work for your application here.
+        }
+
+        private void Refresh_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Refresh button works!");
+            //Do work for your application here.
+        }
+        private void Settings_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Settings button works!");
+            NavigationService.Navigate(new Uri("/Settings.xaml", UriKind.Relative));
+            //Do work for your application here.
+        }
+
+        private void About_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("About works!");
+            //Do work for your application here.
+        }
+		
+        //private void CurrentLB_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        //{
+        //    MessageBox.Show("CurrentLB Selection works!");
+        //    // TODO: Add event handler implementation here.
+        //}
+        //private void AlertLB_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        //{
+        //    MessageBox.Show("AlerLB Selection works!");
+        //    // TODO: Add event handler implementation here.
+        //}
+    }
+}
---
+++ localChat/latLong.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace localChat
+{
+    class latLong
+    {
+        private static int REMOVE_DECIMAL = 100000;
+        private static int LAT_ADD = 90, LON_ADD = 180;
+        private static float LAT_SEGMENT = 1, LON_SEGMENT = 1;
+
+        private float lat, lon;
+        private float sysLat, sysLon;
+        private string gridP = "p.", grid1 = "1.", grid2 = "2.", grid3 = "3.", grid4 = "4."
+            , grid5 = "5.", grid6 = "6.", grid7 = "7.", grid8 = "8.";
+
+        latLong(float lat, float lon)
+        {
+            //Set variables
+            this.lat = lat;
+            this.lon = lon;
+            sysLat = this.lat + LAT_ADD + REMOVE_DECIMAL;
+            sysLon = this.lon + LON_ADD + REMOVE_DECIMAL;
+
+            //Calculate local sysLat/Lon without minutes (degrees only)
+            //Use to calculate grids
+            float sysLatDeg = (float)( (int)( this.lat + LAT_ADD ) );
+            float sysLonDeg = (float)( (int)( this.lon + LON_ADD ) );
+
+            gridP += ( (int)( sysLatDeg / LAT_SEGMENT ) ).ToString();
+            gridP += ".";
+            gridP += ( (int)(sysLonDeg / LON_SEGMENT) ).ToString();
+
+            gridP += ((int)( ( sysLatDeg - ( LAT_SEGMENT / 2 ) ) / LAT_SEGMENT)).ToString();
+            gridP += ".";
+            gridP += ((int)(sysLonDeg / LON_SEGMENT)).ToString();
+        }
+    }
+}
