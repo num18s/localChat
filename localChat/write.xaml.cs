ebe5231eb0748292113d9946e9270c95bfd002b2
num18s <bohaos@gmail.com>
2013-07-23 23:10:19 -0700

    Added slid bar support
    Note: the bar is now in Meters!!!
    10 Meters to 10K Meters

--- localChat/write.xaml.cs
+++ localChat/write.xaml.cs
@@ -15,6 +15,9 @@ namespace localChat
     {
         bool gotId = false;
         String myId = String.Empty;
+        private Microsoft.Phone.Shell.ProgressIndicator pi;
+
+        bool pageLoaded = false;
 
         public WritePage()
         {
@@ -26,6 +29,7 @@ namespace localChat
                 myId = Convert.ToBase64String(myDeviceID);
                 gotId = true;
             }
+            pageLoaded = true;
         }
 
         private void Post_Button_Click(object sender, RoutedEventArgs e)
@@ -33,28 +37,43 @@ namespace localChat
             String title = this.Message_Title_Box.Text;
             String message = this.Message_Post_Box.Text;
             int id = App.ReadMsgList.CurrentItemCount();
-			int radiusMeters = 100;
+            int radiusMeters = (int)Distance_Slider.Value;
 
-            // MessageItem outgoingMsg = new MessageItem()
-                    // {
-                        // ID = id.ToString(),
-                        // Date = DateTime.Now.ToString("MM/dd/yyyy"),
-                        // Time = DateTime.Now.ToString("HH:mm:ss tt"),
-                        // Title = title,
-                        // Author = myId,
-                        // Msg = message
-                    // };
+            MessageItem outgoingMsg = new MessageItem()
+                     {
+                         ID = id.ToString(),
+                         Date = DateTime.Now.ToString("MM/dd/yyyy"),
+                         Time = DateTime.Now.ToString("HH:mm:ss tt"),
+                         Title = title,
+                         Author = myId,
+                         Msg = message
+                     };
 
 			/* progresss bar... */
-			
+            pi = new Microsoft.Phone.Shell.ProgressIndicator();
+            pi.IsIndeterminate = true;
+            pi.Text = "Posting message, please wait...";
+            pi.IsVisible = true;
+            Microsoft.Phone.Shell.SystemTray.SetIsVisible(this, true);
+            Microsoft.Phone.Shell.SystemTray.SetProgressIndicator(this, pi);
+
 			/* wait for message back from post to the clod.. */
             dataSource ds = new dataSource(myId);
 			ds.write(radiusMeters, title, message);
-
-            //App.ReadMsgList.Items.Add(outgoingMsg);
+                        
+            App.ReadMsgList.Items.Add(outgoingMsg);
 
             // Navigate to the new page
             NavigationService.Navigate(new Uri("/ReadLongListPage.xaml", UriKind.Relative));
+        }
+
+        private void Distance_Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (pageLoaded)
+            {
+                int radiusMiles = (int)Distance_Slider.Value;
+                curMileValue.Text = radiusMiles.ToString();
+            }
         }
     }
 }
