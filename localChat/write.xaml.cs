f1fd5c1c87cba6d17308edf52945809f514f551d
num18s <bohaos@gmail.com>
2013-07-27 21:54:42 -0700

    Bug fix on ReadLongList page and added post confirm on
    Fixed the bug where receiving post is adding messages that this already
    pulled last time when the page is refreshed.
    Add message post confirm popup in case the user accidently push the post
    button on write page.

--- localChat/write.xaml.cs
+++ localChat/write.xaml.cs
@@ -35,42 +35,46 @@ namespace localChat
 
         private void PostButton_Click(object sender, EventArgs e)
         {
-            String title = this.Message_Title_Box.Text;
-            String message = this.Message_Post_Box.Text;
-            int id = App.ReadMsgList.CurrentItemCount();
-            int radiusMeters = (int)Distance_Slider.Value;
+            MessageBoxResult postMsg = MessageBox.Show("Please click ok if you really want to post this message?", "Posting Message?", MessageBoxButton.OKCancel);
+            if (postMsg == MessageBoxResult.OK)
+            {
+                String title = this.Message_Title_Box.Text;
+                String message = this.Message_Post_Box.Text;
+                int id = App.ReadMsgList.CurrentItemCount();
+                int radiusMeters = (int)Distance_Slider.Value;
 
-            MessageItem outgoingMsg = new MessageItem()
-                     {
-                         dbMsgID = id.ToString(),
-                         Date = DateTime.Now.ToString("MM/dd/yyyy"),
-                         Time = DateTime.Now.ToString("HH:mm:ss tt"),
-                         Title = title,
-                         Author = myId,
-                         Msg = message
-                     };
+                MessageItem outgoingMsg = new MessageItem()
+                         {
+                             dbMsgID = id.ToString(),
+                             Date = DateTime.Now.ToString("MM/dd/yyyy"),
+                             Time = DateTime.Now.ToString("HH:mm:ss tt"),
+                             Title = title,
+                             Author = myId,
+                             Msg = message
+                         };
 
-            /* progresss bar... */
-            pi = new Microsoft.Phone.Shell.ProgressIndicator();
-            pi.IsIndeterminate = true;
-            pi.Text = "Posting message, please wait...";
-            pi.IsVisible = true;
-            Microsoft.Phone.Shell.SystemTray.SetIsVisible(this, true);
-            Microsoft.Phone.Shell.SystemTray.SetProgressIndicator(this, pi);
+                /* progresss bar... */
+                pi = new Microsoft.Phone.Shell.ProgressIndicator();
+                pi.IsIndeterminate = true;
+                pi.Text = "Posting message, please wait...";
+                pi.IsVisible = true;
+                Microsoft.Phone.Shell.SystemTray.SetIsVisible(this, true);
+                Microsoft.Phone.Shell.SystemTray.SetProgressIndicator(this, pi);
 
-            ThreadStart starter = delegate { Post_Button_Click_Work(title, message, id, radiusMeters); };
+                ThreadStart starter = delegate { Post_Button_Click_Work(title, message, id, radiusMeters); };
 
-            Thread work = new Thread(starter);
-            work.Start();
+                Thread work = new Thread(starter);
+                work.Start();
 
-            ///* wait for message back from post to the cloud.. */
-            //dataSource ds = new dataSource(myId);
-            //ds.write(radiusMeters, title, message);
+                ///* wait for message back from post to the cloud.. */
+                //dataSource ds = new dataSource(myId);
+                //ds.write(radiusMeters, title, message);
 
-            //App.ReadMsgList.Items.Add(outgoingMsg);
+                //App.ReadMsgList.Items.Add(outgoingMsg);
 
-            // Navigate to the new page
-            NavigationService.Navigate(new Uri("/ReadLongListPage.xaml", UriKind.Relative));
+                // Navigate to the new page
+                NavigationService.Navigate(new Uri("/ReadLongListPage.xaml", UriKind.Relative));
+            }
         }
 
         private void Post_Button_Click_Work(string title, string message, int id, int radiusMeters)
